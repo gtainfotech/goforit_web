7a30869a11d45e52348764977d78483ffbb232c7
Anmol <anmolsingh9898@gmail.com>
2019-08-04 19:23:18 -0400

    Auto stash before revert of "Foundation installed"

--- Alpha/0.0.1/server/client/src/App.js
+++ Alpha/0.0.1/server/client/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import LandingPage1 from './landingPage1';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <LandingPage1 />
-
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Go-For-It-Pass. 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
