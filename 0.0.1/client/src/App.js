034dff654c03e7e28bcebb4c05b9643ef9bbeb56
Jaspreet Kaur <jaspreetr86@gmail.com>
2019-08-13 23:30:13 -0400

    changed folder structure

---
+++ 0.0.1/client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import HomePage from './pages/Home';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+      <HomePage />
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
